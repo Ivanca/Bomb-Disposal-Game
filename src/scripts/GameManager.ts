d798a1e2b575fcfeda7cdb043c4cd6fef110e9b4
Ivan Castellanos <ivanca@gmail.com>
2018-02-17 20:43:26 -0500

    score API and HTML+CSS

--- src/scripts/GameManager.ts
+++ src/scripts/GameManager.ts
@@ -2,19 +2,21 @@ import BombsSpawnManager from './BombsSpawnManager';
 import BinsSpawnManager from './BinsSpawnManager';
 import Bin from './bin';
 
-
-
 namespace GameManager {
 
 	let bombsSpawnManager:BombsSpawnManager;
 	let binsSpawnManager:BinsSpawnManager;
 	let bins:Bin[];
 	let gameContainer:HTMLDivElement;
-	let score:HTMLDivElement;
+	let scoreElement:HTMLDivElement;
+	let score:number = 0;
 
 	export function initialSetup() {
 		// code...
 		gameContainer = document.querySelector('#game') as HTMLDivElement;
+		scoreElement = document.createElement('div');
+		scoreElement.classList.add('score');
+		gameContainer.appendChild(scoreElement);
 		bombsSpawnManager = new BombsSpawnManager(gameContainer);
 		binsSpawnManager = new BinsSpawnManager(gameContainer);
 	}
@@ -22,6 +24,11 @@ namespace GameManager {
 	export function gameOver() {
 		bombsSpawnManager.onGameOver();
 		binsSpawnManager.onGameOver();
+	}
+
+	export function addToScore(num:number) {
+		score += num;
+		scoreElement.innerHTML = "Score: " + score;
 	}
 }
 
